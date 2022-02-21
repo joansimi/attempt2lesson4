fb08ef17e5119f3ec446b29472582f1496cd6a01
joansimi <97364180+joansimi@users.noreply.github.com>
2022-02-22 00:14:17 +0300

    implemented the conversion links and set the weather icon

--- src/index.js
+++ src/index.js
@@ -36,6 +36,10 @@ function displayWeather(response) {
     response.data.weather[0].main;
   document.querySelector(".humidity").innerHTML = response.data.main.humidity;
   document.querySelector(".wind").innerHTML = response.data.wind.speed;
+  celsiustemperature=Math.round(
+    response.data.main.temp)
+    let iconelement=document.querySelector("#icon-image");
+    iconelement.setAttribute("src", "https://openweathermap.org/img/wn/"+response.data.weather[0].icon+"@2x.png")
 }
 
 function weatherExtract(weather) {
@@ -72,3 +76,23 @@ function getposition(position) {
 
 let currentWeather = document.querySelector("#current-location");
 currentWeather.addEventListener("click", myWeather);
+
+let celsiustemperature=null;
+
+function converttofahrenheit(event){
+  event.preventDefault();
+  document.querySelector(".temp").innerHTML=Math.round((celsiustemperature * 9/5) + 32);
+}
+
+let fahrenheitElement= document.querySelector("#fahrenheit-link");
+fahrenheitElement.addEventListener("click", converttofahrenheit);
+
+function converttocelsius(event){
+  event.preventDefault();
+  document.querySelector(".temp").innerHTML=celsiustemperature;
+}
+
+let celsiusElement= document.querySelector("#celsius-link");
+celsiusElement.addEventListener("click", converttocelsius)
+
+
