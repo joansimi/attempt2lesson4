38dc473a766ce9d3e03b545d22fa37db010f15c2
joansimi <97364180+joansimi@users.noreply.github.com>
2022-03-02 20:47:05 +0300

    Added weather forecast html and js

--- src/index.js
+++ src/index.js
@@ -27,6 +27,57 @@ currenttime.innerHTML = day + " " + time;
 let form = document.querySelector(".search-form");
 form.addEventListener("submit", weatherExtract);
 
+function formatDay(timestamp){
+  let date=new Date(timestamp * 1000);
+  let days = [
+    "Sun",
+    "Mon",
+    "Tue",
+    "Wed",
+    "Thur",
+    "Fri",
+    "Sat"
+  ];
+  let day = days[date.getDay()];
+  return(day)
+
+}
+
+function displayForecast(response){
+  let forecast= response.data.daily;
+  let forecastElement =document.querySelector("#weatherforecast")
+  let forecastHTML=`<div class="row">`;
+  forecast.forEach(function (forecastDay, index) {
+    if (index<6) {
+         
+    forecastHTML= forecastHTML +
+    `<div class="col-2">
+    <ul style="text-align: center;">
+      <li style="color:#45171d" >
+        ${formatDay(forecastDay.dt)}
+      </li>
+      <li>
+      <img src="http://openweathermap.org/img/wn/${forecastDay.weather[0].icon}@2x.png" alt="icon" width="50">
+      </li>
+      <li>
+        <span style="color:#e84a5f;">${forecastDay.temp.max}</span> <span style="color:#ff847c;">${forecastDay.temp.min}</span>
+      </li>
+    </ul>
+  </div>`;
+  
+    }
+  });
+  forecastHTML= forecastHTML + `</div>`;
+  forecastElement.innerHTML=forecastHTML;
+
+
+}
+function forecastExtract(coordinates){
+ let apiKey="0aacb9bf31eaeb6f35035fe23576f7b2";
+ let forecastUrl=`https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&units=metric&appid=${apiKey}`
+ axios.get(forecastUrl).then(displayForecast)
+}
+
 function displayWeather(response) {
   document.querySelector("h1").innerHTML = response.data.name;
   document.querySelector(".temp").innerHTML = Math.round(
@@ -40,7 +91,7 @@ function displayWeather(response) {
     response.data.main.temp);
     let iconelement=document.querySelector("#icon-image");
     iconelement.setAttribute("src", "https://openweathermap.org/img/wn/"+response.data.weather[0].icon+"@2x.png");
-   
+  forecastExtract(response.data.coord)
 }
 
 function weatherExtract(weather) {
